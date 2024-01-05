8028268a8d6f2a8564cfaf58a7ded207c0b8b705
Mahesh Mamilla <mahesh.mamilla@kyndryl.com>
2024-01-05 16:47:56 +0530

    added comment

---
+++ app.py
@@ -0,0 +1,12 @@
+# app.py
+#comment
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<h1>Hello, Azure!</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
